dd6905c6181b1a38a19c8e33e1fba2b1fd645704
Kieran Kay <kierankay@gmail.com>
2019-09-12 14:27:43 -0700

    added JWTauth and ensureloggedin middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,1 +1,27 @@
-"SUP"
+const jwt = require('jsonwebtoken');
+const {SECRET_KEY} = require('../config')
+const ExpressError = require('../helpers/expressError')
+
+function authenticateJWT(req, res, next) {
+  try {
+    const tokenFromBody = req.body._token;
+    const payload = jwt.verify(tokenFromBody, SECRET_KEY);
+    req.user = payload; // create a current user
+    return next();
+  } catch (err) {
+    return next();
+  }
+}
+
+function ensureLoggedIn(req, res, next) {
+  if (!req.user) {
+    return next({ status: 401, message: "Unauthorized" });
+  } else {
+    return next();
+  }
+}
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn
+}
